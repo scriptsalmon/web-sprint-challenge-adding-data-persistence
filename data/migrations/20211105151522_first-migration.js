6d163788f1a080e395bceaa8df74ffcb70fc07d6
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-05 08:35:34 -0700

    database fleshed out

--- data/migrations/20211105151522_first-migration.js
+++ data/migrations/20211105151522_first-migration.js
@@ -1,10 +1,35 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('project', (table) => {
-    table.increments('project_id');
-  })
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("project", (table) => {
+      table.increments("project_id");
+      table.string("project_name", 64).notNullable();
+      table.string("project_description", 128);
+      table.boolean("project_completed");
+    })
+    .createTable("resources", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name", 64).notNullable().unique();
+      table.string("resource_description", 128);
+    })
+    .createTable("task", (table) => {
+      table.increments("task_id");
+      table.string("task_description", 128).notNullable();
+      table.string("task_notes", 256);
+      table.boolean("task_completed");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('project');
+    .dropTableIfExists("task")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("project");
 };
